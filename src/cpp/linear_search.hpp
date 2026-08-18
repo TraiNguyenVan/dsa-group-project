e3dddfccfe02fe4a5b099d56433881ba12468618
TraiNguyenVan <traipupgmobile@gmail.com>
2026-08-18 23:11:19 +0700

    feat: implement simple linear search algorithm

---
+++ src/cpp/linear_search.hpp
@@ -0,0 +1,13 @@
+// Simple implementation of linear search
+
+#include <iostream>
+
+using namespace std;
+
+int linear_search(int a[], int n, int val) {
+    for (int i = 0; i < n; i++) {
+        if (a[i] == val)
+            return i;
+    }
+    return -1;
+}
