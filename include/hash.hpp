ee25d0d8241894866e954eeef9acfb926af4387a
TraiNguyenVan <traipupgmobile@gmail.com>
2026-08-28 22:59:38 +0700

    fix: fix some bro and me mistakes

--- include/hash.hpp
+++ include/hash.hpp
@@ -2,7 +2,6 @@
 #define HASH_HPP
 #include <iostream>
 
-#include <vector>
 using namespace std;
 
 namespace HashTable {
@@ -11,25 +10,19 @@ class DivisionMethod {
    private:
     static const int SIZE = 10;
     int table[SIZE];
-
    public:
-    int HashTable() {
+    DivisionMethod() {
         for (int i = 0; i < SIZE; ++i) {
             table[i] = -1;
         }
     }
-
     int hashFunction(int key) {
         return key % SIZE;
     }
-    // phần insert này thì là đề trực tiếp dữ liệu lên cái đã có
-    //
     void insert(int key) {
         int index = hashFunction(key);
         table[index] = key;
     }
-    // collison occur when hashFunction return the same index () on different keys
-    // Tinh xac suat the 2 so ngau nhien sau khi qua hashFunction tra ve 2 index bang nhau
     bool search(int key) {
         int index = hashFunction(key);
         return (index != -1) ? index : 0;
@@ -41,16 +34,6 @@ class DivisionMethod {
     }
 };
 
-// like iam looking ughh like how we do on our project🗿🗿❓
-// igig, i think only function in hpp and how it work in cpp? ughhh
-// sir my lapdih is about to die so imma push da code and bro yesyesyes pipilabu
-// poop1
-// like only me and bro how to git bro what now
-// can bro commit things
-// 🐧 can we do it here
-// yes bro have source control tab and a terminal
-// how did it just ask me if I want to see 114 possibilities? Am I Doctor Strange?
-// bro like when bro tab in empty space its gonna ask🐧 
 class LinearProbing {};
 
 class MidSquare {
