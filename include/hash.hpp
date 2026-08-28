9740533be9eee335af289cee4ad7b080c8ae63cf
TraiNguyenVan <traipupgmobile@gmail.com>
2026-08-29 00:01:28 +0700

    fix: classify hashtable implementations in a better way:)

--- include/hash.hpp
+++ include/hash.hpp
@@ -6,12 +6,13 @@ using namespace std;
 
 namespace HashTable {
 
-class DivisionMethod {
-   private:
+class LinearProbing {
+private:
     static const int SIZE = 10;
     int table[SIZE];
-   public:
-    DivisionMethod() {
+
+public:
+    LinearProbing() {
         for (int i = 0; i < SIZE; ++i) {
             table[i] = -1;
         }
@@ -21,11 +22,25 @@ class DivisionMethod {
     }
     void insert(int key) {
         int index = hashFunction(key);
+
+        while (table[index] != -1) {
+            index = (index + 1) % SIZE;
+        }
+
         table[index] = key;
     }
     bool search(int key) {
         int index = hashFunction(key);
-        return (index != -1) ? index : 0;
+
+        while (table[index] != -1) {
+            if (table[index] == key) {
+                return true;
+            }
+
+            index = (index + 1) % SIZE;
+        }
+
+        return false;
     }
     void print() {
         for (int i = 0; i < SIZE; i++) {
@@ -34,14 +49,13 @@ class DivisionMethod {
     }
 };
 
-class LinearProbing {};
-
 class MidSquare {
-    private:
-        string square(string s);
-        int hashReturn(string s);
-    public:
-        string hash(string s);
+private:
+    string square(string s);
+    int hashReturn(string s);
+
+public:
+    string hash(string s);
     //~~~~~~~~~~~~~~~~~~~~~~~~~
 };
 }  // namespace HashTable
